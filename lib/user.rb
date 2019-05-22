871ec64cb95d6e7a2cf34bd46ca3dd6576558d4f
Keith Isham <kisham8899@gmail.com>
2019-05-22 17:21:10 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,25 @@
+class User
+
+ attr_accessor  :first_name, :last_name
+
+def initialize
+  @knowledge = []
+end
+
+def learn(knowledge)
+@knowledge << knowledge
+
+end
+
+def knowledge
+  return @knowledge
+end
+
+def teach
+   KNOWLEDGE.sample
+end
+
+
+
+
+end
